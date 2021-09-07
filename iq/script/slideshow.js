17f59fbe07d135d760a363ee24a42a70a8300b69
moomfakh <89586272+moomfakh@users.noreply.github.com>
2021-09-07 16:14:11 +0530

    Add files via upload

---
+++ iq/script/slideshow.js
@@ -0,0 +1,37 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+ if (e.keyCode == '37') {
+       // left arrow
+     plusSlides(-1)
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+       plusSlides(1)
+    }
+
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("con");
+  if (n < 1) {slideIndex = 1}
+  if (n > slides.length) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+slides[slideIndex-1].style.display = "block";
+}
